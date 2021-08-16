841be82f87ba93765148318b955046cc008c7b79
Murmeko <yigiterdinc@gmail.com>
2021-08-16 19:50:30 +0300

    Created MVVM skeleton.

---
+++ DH Video Lite/Constants.swift
@@ -0,0 +1,8 @@
+//
+//  Constants.swift
+//  DH Video Lite
+//
+//  Created by Yiğit Erdinç on 16.08.2021.
+//
+
+import Foundation
---
+++ DH Video Lite/Network/DHVideosAPI.swift
@@ -0,0 +1,8 @@
+//
+//  DHVideosAPI.swift
+//  DH Video Lite
+//
+//  Created by Yiğit Erdinç on 16.08.2021.
+//
+
+import Foundation
